1028045a6014950065b7735497e70891ed7ba944
Yumeto Inaoka <in@okayu.jp>
2018-09-09 16:20:23 +0900

    感情分析結果に応じた返答を実装

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ const lineClient = new line.Client(lineConfig);
 const gcp_key = process.env.GCP_KEY;
 const gcp_analyze_sentiment_url = `https://language.googleapis.com/v1/documents:analyzeSentiment?key=${gcp_key}`;
 
+const replies = [//入力の感情がネガティブであるほど上の返事をする
+  "こんなにひどいことがあったんだから後は上がるだけだよ", // 入力が最もネガティブ
+  "出来ることなら変わってあげたいんだけど……",
+  "元気出して",
+  "明日は明日の風が吹くよ",
+  "まあそんな日もあるさ", 
+  "ふむふむ", // ニュートラル
+  "へー良かったねー",
+  "何だい自慢かい？",
+  "運が良かっただけでしょ",
+  "何て脳天気なんだ",
+  "こんなに良いことがあったんだから後は落ちるだけだよ", // 入力が最もポジティブ
+];
+
 async function createReplyMessage(input) {
   const params = {
     document: {
@@ -22,15 +36,20 @@ async function createReplyMessage(input) {
     encodingType: "UTF8"
   };
 
+  // ここを参考にした
+  // http://tech.wonderpla.net/entry/2017/11/28/110000
   const data = await axios.post(
     gcp_analyze_sentiment_url,
     params
   );
   const document_sentiment_score = data.data.documentSentiment.score;
 
+  // document_sentiment_score は -1 〜 +1 まで0.1刻み
+  // reply_index は 0 〜 10 になる
+  const reply_index = parseInt((document_sentiment_score + 1) / 0.2);
   return {
     type: "text",
-    text: `${document_sentiment_score}`
+    text: replies[reply_index]
   };
 }
 
